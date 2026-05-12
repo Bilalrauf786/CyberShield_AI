17eb60318023bca16d7349b1fb1c8fb9cbed908a
Bilalrauf786 <bilallrauff@gmail.com>
2026-05-12 16:36:28 -0700

    fix streamlit deploy issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="CyberShield AI", layout="wide")
+st.title("🔥 CyberShield AI")
 
-st.title("🔥 CyberShield AI SOC Dashboard")
-st.subheader("Real-Time Cyber Security Monitoring System")
+st.success("App is running successfully 🚀")
 
-st.success("System Running Successfully 🚀")
-
-st.write("Live attacks, logs aur ML detection yahan show hoga.")
+st.write("If you see this, deployment is working.")
