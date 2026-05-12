540f9ab258ce7cfa6153f8f7ab515411e453b79a
Bilalrauf786 <bilallrauff@gmail.com>
2026-05-12 16:31:29 -0700

    add streamlit entry file

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="CyberShield AI", layout="wide")
+
+st.title("🔥 CyberShield AI SOC Dashboard")
+st.subheader("Real-Time Cyber Security Monitoring System")
+
+st.success("System Running Successfully 🚀")
+
+st.write("Live attacks, logs aur ML detection yahan show hoga.")
